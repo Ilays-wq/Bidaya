568d778c71aa7bcdeb98e99378f0f8520e6f9128
Ilays-wq <Ilaysin@mail.ru>
2025-01-15 22:20:43 +0300

    Add files via upload

---
+++ 'module_1_3.py
@@ -0,0 +1,11 @@
+name = 'Ilays'
+print(name,type(name))
+name = 1
+print(name,type(name))
+name = 5.5
+print(name,type(name))
+name={1, 2}
+print(name,type(name))
+age = 30
+new_age = 20
+print(age+new_age)
